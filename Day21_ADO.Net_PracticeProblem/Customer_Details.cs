03db8e81d4f7c4e749f0e35c9c57d007e73a6e6a
hareesh <hareesh.vanapalli.888@gmail.com>
2023-10-20 12:00:35 +0530

    [hari]

---
+++ Day21_ADO.Net_PracticeProblem/Customer_Details.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADO.Net_PracticeProblem
+{
+    public class Customer_Details
+    {
+        public static void AddCustomer(string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("INSERT INTO Customer (CustomerName, Phone, Address) VALUES (@CustomerName, @Phone, @Address)", connection))
+                {
+                    command.Parameters.AddWithValue("@CustomerName", "John Doe");
+                    command.Parameters.AddWithValue("@Phone", "555-123-4567");
+                    command.Parameters.AddWithValue("@Address", "123 Main St");
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
